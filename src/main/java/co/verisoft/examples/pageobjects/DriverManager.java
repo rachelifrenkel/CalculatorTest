e36521b7a576ac1519c35ddc91bfee5d9b3fd152
rachel <f0533130872@gmail.com>
2023-09-14 05:02:12 +0300

    fix driverManager

--- src/main/java/co/verisoft/examples/pageobjects/DriverManager.java
+++ src/main/java/co/verisoft/examples/pageobjects/DriverManager.java
@@ -7,15 +7,18 @@ import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Properties;
 
 public class DriverManager {
     PropertiesService propertiesService = new PropertiesService();
-    String platformName = propertiesService.getProperty("mobileCapability.properties").getProperty("PLATFORM_NAME");
-    String platformVersion = propertiesService.getProperty("mobileCapability.properties").getProperty("PLATFORM_VERSION");
-    String deviceName = propertiesService.getProperty("mobileCapability.properties").getProperty("DEVICE_NAME");
-    String appActivity = propertiesService.getProperty("androidMobileCapability.properties").getProperty("APP_ACTIVITY");
-    String appPackage = propertiesService.getProperty("androidMobileCapability.properties").getProperty("APP_PACKAGE");
-    String url = propertiesService.getProperty("appiumServer.properties").getProperty("URL");
+    Properties mobileCapability = propertiesService.getProperty("src/test/resources/mobileCapability.properties");
+    String platformName = mobileCapability.getProperty("PLATFORM_NAME");
+    String platformVersion = mobileCapability.getProperty("PLATFORM_VERSION");
+    String deviceName = mobileCapability.getProperty("DEVICE_NAME");
+    Properties androidMobileCapability = propertiesService.getProperty("src/test/resources/androidMobileCapability.properties");
+    String appActivity = androidMobileCapability.getProperty("APP_ACTIVITY");
+    String appPackage = androidMobileCapability.getProperty("APP_PACKAGE");
+    String url = propertiesService.getProperty("src/test/resources/appiumServer.properties").getProperty("URL");
 
     public AppiumDriver driverInit() throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
