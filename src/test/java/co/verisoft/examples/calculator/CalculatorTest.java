05fc7522fb4f18a77a72c6a0d4525cd4c44f494e
rachel <f0533130872@gmail.com>
2023-09-14 10:32:44 +0300

    fix test anotaion

--- src/test/java/co/verisoft/examples/calculator/CalculatorTest.java
+++ src/test/java/co/verisoft/examples/calculator/CalculatorTest.java
@@ -1,10 +1,11 @@
 package co.verisoft.examples.calculator;
 import co.verisoft.examples.pageobjects.CalculatorPage;
 import co.verisoft.examples.pageobjects.CalculatorService;
-import org.junit.Test;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import java.net.MalformedURLException;
 
 public class CalculatorTest extends BaseTest {
@@ -22,7 +23,6 @@ public class CalculatorTest extends BaseTest {
             driver.quit();
         }
     }
-
     @Test
     @DisplayName("Adds two numbers and checks the result")
     public void testCalculator() {
